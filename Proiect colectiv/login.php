90e41ed6320c9202cc9817864731f06b4c356367
OanaBN <lolly.lu@yahoo.com>
2024-04-01 02:22:50 +0300

    ALE SA DAI PULL, AM FACUT DOAR LOGININURILE

--- Proiect colectiv/login.php
+++ Proiect colectiv/login.php
@@ -18,31 +18,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logare</title>
-    <style>
-        /* Stilizare CSS poate fi adăugată aici */
-        .div {
-            width: 300px;
-            margin: 0 auto;
-            padding: 20px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-        input[type="email"],
-        input[type="password"],
-        input[type="submit"] {
-            width: 100%;
-            margin-bottom: 10px;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-    </style>
+    <link rel="stylesheet" href="login.css">
+    <title>Log in</title>
+
 </head>
 <body>
 
-<div class="div">
-    <h2>Logare</h2>
+<div class="form-container">
+    <h2 class="form-message">Log in</h2>
     <?php if (count($errors) > 0) : ?>
         <div style="color: red;">
             <?php foreach ($errors as $error) : ?>
@@ -56,13 +39,12 @@
 
     <form method="post">
         <input type="email" name="email" placeholder="Email" required><br>
-        <input type="password" name="password" placeholder="Parolă" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
         <br>
-        <input type="submit" value="Logare">
+        <input type="submit" value="Log in" class="signup-button">
+        <input type="button" value="No account? Sign up here!" class="signup-button" onclick="window.location.href = 'signup.php';">  
     </form>
     <br>
-
-    <p>Nu ai cont? <a href="signup.php">Înregistrează-te aici</a></p>
 </div>
 
 </body>
